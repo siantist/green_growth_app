db6a0e2f64b64257db58b2865b281c8f2e8a67a1
siantist <104287799+siantist@users.noreply.github.com>
2024-01-01 17:05:27 -0800

    Create UsersView.swift

---
+++ Views/UsersView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct UsersView: View {
+  @State var users: [UserModel] = [
+  UserModel(displayName:"Nick", userName:"nick123", followerCount:100, points:100, isVerified:true)
+  UserModel(displayName:"Serena", userName:"serena456", followerCount:10, points:200, isVerified:true) ]
+
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(users) { user in 
+        
+        }
+      }
+    }
+  }
+}
