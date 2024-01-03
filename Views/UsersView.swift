e456b88b108e4e8f7d168bdd6045e224c9969d25
siantist <104287799+siantist@users.noreply.github.com>
2024-01-03 14:33:32 -0800

    Update UsersView.swift

--- Views/UsersView.swift
+++ Views/UsersView.swift
@@ -9,7 +9,7 @@ struct UserModel {
 
 struct UsersView: View {
   @State var users: [UserModel] = [
-  UserModel(displayName:"Nick", userName:"nick123", followerCount:100, points:100, isVerified:true)
+  UserModel(displayName:"Nick", userName:"nick123", followerCount:100, points:100, isVerified:true),
   UserModel(displayName:"Serena", userName:"serena456", followerCount:10, points:200, isVerified:true) ]
 
   var body: some View {
