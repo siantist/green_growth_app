17e69a54809852c99d2a883161948d302a356819
siantist <104287799+siantist@users.noreply.github.com>
2024-01-06 17:18:46 -0800

    Update Swift_Rest_API.swift

--- Swift_Rest_API.swift
+++ Swift_Rest_API.swift
@@ -8,3 +8,9 @@ struct Question: Codable {
   let score: Int
   let title: String
 }
+func performAPICall() async throws -> Question {
+  let url = URL(string: "")
+  let (data, _) = try await URLSession.shared.data(from: url)
+  let wrapper = try JSONDecoder().decode(Wrapper.self, from: data)
+  return wrapper.items[0]
+}
