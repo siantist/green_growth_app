54e170998d0eb23228329096b982a71f4d21685c
siantist <104287799+siantist@users.noreply.github.com>
2024-01-20 11:18:05 -0800

    Create LocationView.swift

---
+++ Views/LocationView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct LocationView: View {
+  var body: some View {
+    Text("Welcome to Location View")
+  }
+}
+
+struct LocationView_Previews: PreviewProvider {
+  static var previews: some View {
+    LocationView()
+  }
+}
