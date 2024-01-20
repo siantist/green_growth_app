713631ac79f734b6932477afdebdb560ff4d390a
siantist <104287799+siantist@users.noreply.github.com>
2024-01-20 10:32:29 -0800

    Create Location.swift

---
+++ Model/Location.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Location {
+  let name: String
+  let cityName: String
+  let coordinates: CLLocationCoordinate2D
+  //let description: String
+  //let imageName: [String]
+  //let link: String
+}
