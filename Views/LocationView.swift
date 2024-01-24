19ec7dcfe1b2ed4c3b73586a0b27d21ff132d29b
siantist <104287799+siantist@users.noreply.github.com>
2024-01-23 17:40:07 -0800

    Update LocationView.swift

--- Views/LocationView.swift
+++ Views/LocationView.swift
@@ -1,13 +1,32 @@
 import SwiftUI
 
+class LocationViewModel: ObservableObject {
+  @Published var locations: [Location]
+
+  init() {
+    let locations = LocationDataService.locations
+    self.locations = locations
+  }
+}
+
 struct LocationView: View {
+
+  @StateObject private var vm = LocationViewModel()
+  @EnvironmentObject private var vm : LocationViewModel
+  
   var body: some View {
-    Text("Welcome to Location View")
+    Text("Welcome to Location View"),
+    List {
+      ForEach(vm.locations) {
+        Text($0.name)
+      }
+    }
   }
 }
 
 struct LocationView_Previews: PreviewProvider {
   static var previews: some View {
     LocationView()
+      .environmentObject(LocationViewModel())
   }
 }
