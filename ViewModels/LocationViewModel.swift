b0f3d67c6ddb30bffc5e4cba9cd313f831dfe91f
siantist <104287799+siantist@users.noreply.github.com>
2024-01-23 21:40:40 -0800

    Update LocationViewModel.swift

--- ViewModels/LocationViewModel.swift
+++ ViewModels/LocationViewModel.swift
@@ -2,8 +2,28 @@ import Foundation
 
 class LocationViewModel: ObservableObject {
   @Published var locations: [Location]
+
+  // current location on map
+  @Published var mapLocation: Location {
+    didSet {
+      updateMapRegion(location: mapLocation)
+    }
+  }
+
+  @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
+  let mapSpan = MKCoordinateSpan(latitudeDelta:0.1, longitudeDelta: 0.1)  
   init() {
     let locations = LocationsDataService.locations
     self.locations = locations
+    self.mapLocation = locations.first!
+    self.updateMapRegion(location: locations.first!)
+  }
+  private func updateMapRegion(location: Location) {
+    withAnimation(.easeInOut) {
+      mapRegion = MKCoordinateRegion(
+        center: location.coordinates,
+        span: mapSpan
+      )
+    }
   }
 }
